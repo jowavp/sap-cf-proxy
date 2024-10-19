9d5c277885e7e8cd2d22f6a38ff9b33233484f14
Ariel Bravo Ayala <2177550+ambravo@users.noreply.github.com>
2024-10-19 13:33:18 -0300

    refactor: Enhance handling of the onPremisePort environment variable

--- socks-proxy/src/main/java/StartSocksProxy.java
+++ socks-proxy/src/main/java/StartSocksProxy.java
@@ -6,16 +6,16 @@ public class StartSocksProxy {
 
     public static void main(String[] args) throws IOException {
         String onPremiseHost = System.getenv("ON_PREMISE_HOST");
-        String onPremisePort = System.getenv("ON_PREMISE_PORT");
+        int onPremisePort = System.getenv("ON_PREMISE_PORT") != null ? Integer.parseInt(System.getenv("ON_PREMISE_PORT")) : 0;
         String cloudConnectorLocationId = System.getenv("CLOUD_CONNECTOR_LOCATION_ID");
         // default port 5050
         int port = (System.getenv("PORT") != null) ? Integer.parseInt(System.getenv("PORT")) : 5050;
         // default to using SSH tunnel
         boolean useSSHTunnel = (System.getenv("USE_SSH_TUNNEL") == null) || Boolean.parseBoolean(System.getenv("USE_SSH_TUNNEL"));
         
-        if (onPremiseHost != null && onPremisePort != null) {
+        if (onPremiseHost != null && onPremisePort != 0) {
             LOGGER.info("Starting up local SOCKS5 proxy to " + onPremiseHost + ":" + onPremisePort);
-            ProxyServer proxy = new ProxyServer(port, onPremiseHost, Integer.parseInt(onPremisePort), cloudConnectorLocationId, useSSHTunnel);
+            ProxyServer proxy = new ProxyServer(port, onPremiseHost, onPremisePort, cloudConnectorLocationId, useSSHTunnel);
             proxy.run();
         } else {
             LOGGER.severe("Could not find ON_PREMISE_HOST and/or ON_PREMISE_PORT defined in any valid way");
